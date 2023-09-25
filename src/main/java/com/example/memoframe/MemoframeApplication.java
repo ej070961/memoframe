a0a53d0840098c0bf50d1ee2b70b01c007757069
ej070961 <ej070961@gmail.com>
2023-09-25 22:11:50 +0900

    first commit

---
+++ src/main/java/com/example/memoframe/MemoframeApplication.java
@@ -0,0 +1,13 @@
+package com.example.memoframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoframeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoframeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/memoframe/MemoframeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memoframe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoframeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
